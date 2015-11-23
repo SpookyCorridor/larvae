45105bbbdaec99a122f530231e8c9141d70e47e1
Dylan <invision33@gmail.com>
2015-11-23 15:39:40 -0600

    add select2 plugin for tags visual improvement

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,6 +4,7 @@
 	<meta charset="UTF-8">
 	<title>Document</title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.1-rc.1/css/select2.min.css" rel="stylesheet" />
 </head>
 <body>
 	<div class="container">
@@ -13,14 +14,16 @@
 		@yield('content')
 	</div>
 	
-	<footer>
-		@yield('footer')
-	</footer>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-	
+	<script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.1-rc.1/js/select2.min.js"></script>
 	<script>
 		$('div.alert').not('.alert-important').delay(3000).slideUp(300); 
 	</script>
+	
+	<footer>
+		@yield('footer')
+	</footer>
+	
 </body>
 </html>
