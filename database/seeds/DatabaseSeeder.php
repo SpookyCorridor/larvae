73baa9f9e43fe88c1a05fbe290e254a3ca6ba507
Dylan <invision33@gmail.com>
2015-12-01 03:55:02 -0600

    add tag seeding for example

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use App\Tag; 
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,8 +15,11 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        // $this->call(UserTableSeeder::class);
+        Tag::truncate(); 
 
+        factory(Tag::class, 40)->create(); 
+        // $this->call(UserTableSeeder::class);
+        
         Model::reguard();
     }
 }
